690574505034bd070194a5bea85b956f2d72c159
elokaagu <eloka.agu@icloud.com>
2022-01-03 23:56:59 +0000

    styled buttons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,15 @@ function MyApp({ Component, pageProps }: AppProps) {
 import { withTRPC } from "@trpc/next";
 import type { AppRouter } from "@/backend/router";
 
+function getBaseUrl() {
+  if (process.browser) return "";
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+
+  return `
+    http://localhost:${process.env.PORT ?? 3000}
+  `;
+}
+
 export default withTRPC<AppRouter>({
   config({ ctx }) {
     /**
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { getOptionsForVote } from "../utils/getRandomPokemon";
 import { trpc } from "../utils/trpc";
 
+const btn =
+  "inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
+
 export default function Home() {
   const [ids, updateIds] = useState(() => getOptionsForVote());
 
@@ -13,13 +16,19 @@ export default function Home() {
 
   if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
 
+  const voteForRoundest = (selected: number) => {
+    // todo: fire mutation to persist changes
+
+    updateIds(getOptionsForVote());
+  };
+
   console.log(firstPokemon.data);
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is rounder ?</div>
-      <div className="p-2"></div>
+      <div className="p-2" />
       <div className="border rounded-lg p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-64 h-64 flex flex-col mr-10">
+        <div className="w-64 h-64 flex flex-col items-center mr-10">
           <img
             src={firstPokemon.data?.sprites.front_default}
             className="w-full"
@@ -28,9 +37,12 @@ export default function Home() {
           <div className="text-xl text-center pb-4 capitalize mt-[-2rem]">
             {firstPokemon.data?.name}
           </div>
+          <button className={btn} onClick={() => voteForRoundest(first)}>
+            Rounder
+          </button>
         </div>
-        <div className="p8">vs</div>
-        <div className="w-64 h-64 flex flex-col  ml-10">
+        <div className="p-8">vs</div>
+        <div className="w-64 h-64 flex flex-col items-center ml-10">
           <img
             src={secondPokemon.data?.sprites.front_default}
             className="w-full"
@@ -39,7 +51,11 @@ export default function Home() {
           <div className="text-xl text-center pb-4 capitalize mt-[-2rem]">
             {firstPokemon.data?.name}
           </div>
+          <button className={btn} onClick={() => voteForRoundest(second)}>
+            Rounder
+          </button>
         </div>
+        <div className="p-2" />
       </div>
     </div>
   );
